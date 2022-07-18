1a76837bbc2d88b67a74f28252fb3f36f710d50d
Diffo Tchinda William Stève <williamsteve216@gmail.com>
2022-07-18 14:07:04 +0100

    First backend commit

---
+++ src/main/java/dtsw/school/markbackend/MarkbackendApplication.java
@@ -0,0 +1,13 @@
+package dtsw.school.markbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dtsw/school/markbackend/MarkbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dtsw.school.markbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
